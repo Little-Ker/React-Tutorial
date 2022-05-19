15c091245df39ab1d336c8823a61668ec5125085
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-05-19 22:01:08 +0800

    [修改] 導覽頁使用 Link 連結、回最上面 fn

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -1,26 +1,38 @@
+import React from "react"
 import styles from './menu.module.sass'
 import clsx from 'clsx'
+import { Link } from "react-router-dom"
+import PropTypes from 'prop-types'
 
 function Menu(props) {
-    const preData = props.data.prev
-    const nextData = props.data.next
+    const { data } = props
+    const preData = data.prev
+    const nextData = data.next
+
+    const moveTop = () => {
+        window.scrollTo(0, 0)
+    }
 
     return (
         <div className={styles.menu}>
-            <div className={clsx(styles.btn, preData.title === '' && styles.btnHide)}>
+            <Link to={preData.to} onClick={() => {moveTop()}} className={clsx(styles.btn, preData.title === '' && styles.btnHide)}>
                 <div className={styles.arrow}>
                     <p>Prev</p>
                 </div>
                 <p className={styles.txt}>{preData.title}</p>
-            </div>
-            <div className={clsx(styles.btn, styles.rightBtn, nextData.title === '' && styles.btnHide)}>
+            </Link>
+            <Link to={nextData.to} onClick={() => {moveTop()}} className={clsx(styles.btn, styles.rightBtn, nextData.title === '' && styles.btnHide)}>
                 <div className={styles.arrow}>
                     <p>Next</p>
                 </div>
                 <p className={styles.txt}>{nextData.title}</p>
-            </div>
+            </Link>
         </div>
     )
+}
+
+Menu.propTypes = {
+    data: PropTypes.object.isRequired
 }
 
 export default Menu
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,57 +1,98 @@
 import styles from './navbar.module.sass'
 import React, { useState } from "react"
+import { Link } from "react-router-dom"
 import clsx from 'clsx'
+
+const navList2 = [{
+    title: 'CRA',
+    content: [{
+        txt: '安裝 React', to: 'install'
+    }]
+},{
+    title: '基礎套件',
+    content: [{
+        txt: 'sass', to: 'sass'
+    },{
+        txt: 'eslint', to: 'eslint'
+    },{
+        txt: 'clsx', to: 'clsx'
+    }]
+},{
+    title: 'Component',
+    content: [{
+        txt: 'Component 與 prop', to: 'prop'
+    },{
+        txt: 'prop-types', to: 'prop-types'
+    }]
+},{
+    title: 'Hooks',
+    content: [{
+        txt: 'Hook 介紹', to: 'hook'
+    },{
+        txt: 'useState', to: 'useState'
+    },{
+        txt: 'useEffect', to: 'useEffect'
+    },{
+        txt: 'useCallback', to: 'useCallback'
+    }]
+},{
+    title: 'router',
+    content: [{
+        txt: '安裝指令', to: 'router-install'
+    },{
+        txt: '環境調整', to: 'router-environment'
+    },{
+        txt: 'HashRouter', to: 'hashRouter'
+    }]
+},{
+    title: 'redux',
+    content: [{
+        txt: '安裝指令', to: 'redux-install'
+    },{
+        txt: '環境調整', to: 'redux-environment'
+    },{
+        txt: '取得資料', to: 'redux-getData'
+    },{
+        txt: '改變資料事件', to: 'redux-changeData'
+    },{
+        txt: '延遲改資料事件', to: 'redux-asyncChangeData'
+    },{
+        txt: '使用 Axios 取得資料', to: 'redux-axiosGetData'
+    }]
+},{
+    title: '部屬 git pages',
+    content: [{
+        txt: '部屬方法', to: 'gitPages'
+    },{
+        txt: 'bug 與處理', to: 'gitPagesBug'
+    }]
+},{
+    title: '額外套件',
+    content: [{
+        txt: 'Axios', to: 'axios'
+    },{
+        txt: 'Mock js', to: 'mock'
+    },{
+        txt: 'Material UI', to: 'materialUI'
+    },{
+        txt: 'Swiper', to: 'swiper'
+    },{
+        txt: 'Google Font',  to: 'googleFont'
+    }]
+}]
 
 function Navbar() {
     const [titleIndex, setTitleIndex] = useState(0)
     const [chooseTitleIndex, setChooseTitleIndex] = useState(0)
     const [chooseSubTitleIndex, setChooseSubTitleIndex] = useState(0)
 
-    const navList = [{
-        title: 'CRA',
-        content: [
-            '安裝 node.js', '安裝 React'
-        ]
-    },{
-        title: '基礎套件',
-        content: [
-            'sass', 'prop-types', 'eslint', 'clsx'
-        ]
-    },{
-        title: 'Component',
-        content: [
-            'prop', 'state'
-        ]
-    },{
-        title: 'Hooks',
-        content: [
-            'useState', 'useEffect', 'useCallback'
-        ]
-    },{
-        title: 'router',
-        content: [
-            '安裝指令', '環境調整', 'HashRouter'
-        ]
-    },{
-        title: 'redux',
-        content: [
-            '安裝指令', '環境調整', '取得資料', '改變資料事件', '延遲改資料事件', '使用 Axios 取得資料'
-        ]
-    },{
-        title: '部屬 git pages',
-        content: [
-            '部屬方法', 'bug 與處理'
-        ]
-    },{
-        title: '額外套件',
-        content: [
-            'Axios', 'Mock js', 'Material UI', 'Swiper', 'Google Font'
-        ]
-    }]
-
     function clickSubTitle(titleIndex, subIndex) {
         setChooseTitleIndex(titleIndex)
         setChooseSubTitleIndex(subIndex)
+    }
+
+    const moveTop = () => {
+        window.scrollTo(0, 0)
     }
 
     return (
@@ -62,15 +103,17 @@ function Navbar() {
             </div>
             <div className={styles.linkList}>
             {
-                navList.map((item, index) => (
+                navList2.map((item, index) => (
                     <div key={index} className={styles.link}>
                         <p onClick={() => setTitleIndex(index)} className={clsx(styles.title, index === titleIndex && styles.activeTitleShow)}>{item.title}</p>
                         <div className={clsx(styles.subNav, index === titleIndex && styles.activeShow)}>
-                            {
-                                item.content.map((item2, index2) => (
-                                    <p key={index2} onClick={() => clickSubTitle(index, index2)} className={clsx(styles.subTitle, chooseTitleIndex === index && chooseSubTitleIndex === index2 && styles.clickSubTitle)}>{item2}</p>
-                                ))
-                            }
+                            {item.content.map((item2, index2) => (
+                                <Link key={index2}
+                                className={clsx(styles.subTitle, chooseTitleIndex === index && chooseSubTitleIndex === index2 && styles.clickSubTitle)}
+                                onClick={() => {moveTop();clickSubTitle(index, index2)}}
+                                to={item2.to}
+                                >{item2.txt}</Link>
+                            ))}
                         </div>
                     </div>
                 ))
