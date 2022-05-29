be0f9bbedbda810a867105936c8ca7eebc8fe369
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-05-29 17:26:13 +0800

    [修改] 將導覽頁資料 移到 redux

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,87 +1,11 @@
 import styles from './navbar.module.sass'
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 import clsx from 'clsx'
 
-const navList = [{
-    title: 'CRA',
-    content: [{
-        txt: '安裝 React', to: 'install'
-    }]
-},{
-    title: '基礎套件',
-    content: [{
-        txt: 'sass', to: 'sass'
-    },{
-        txt: 'eslint', to: 'eslint'
-    },{
-        txt: 'clsx', to: 'clsx'
-    }]
-},{
-    title: 'Component',
-    content: [{
-        txt: 'Component 與 prop', to: 'prop'
-    },{
-        txt: 'prop-types', to: 'prop-types'
-    }]
-},{
-    title: 'Hooks',
-    content: [{
-        txt: 'Hook 介紹', to: 'hook'
-    },{
-        txt: 'useState', to: 'useState'
-    },{
-        txt: 'useEffect', to: 'useEffect'
-    },{
-        txt: 'useCallback', to: 'useCallback'
-    },{
-        txt: 'useRef', to: 'useRef'
-    }]
-},{
-    title: 'router',
-    content: [{
-        txt: '安裝 router', to: 'router'
-    },{
-        txt: '巢狀路由', to: 'routerNested'
-    },{
-        txt: 'Router Hook', to: 'router-hook/123'
-    },{
-        txt: 'Router Mode', to: 'routerMode'
-    }]
-},{
-    title: 'redux',
-    content: [{
-        txt: '安裝 redux', to: 'redux-install'
-    },{
-        txt: '取得 / 改變 資料', to: 'redux-state'
-    },{
-        txt: '延遲改資料事件', to: 'redux-asyncState'
-    },{
-        txt: '使用 Axios 取得資料', to: 'redux-axios'
-    }]
-},{
-    title: '部屬 git pages',
-    content: [{
-        txt: '部屬方法', to: 'gitPages'
-    },{
-        txt: 'bug 與處理', to: 'gitPagesBug'
-    }]
-},{
-    title: '額外套件',
-    content: [{
-        txt: 'Axios', to: 'axios'
-    },{
-        txt: 'Mock js', to: 'mock'
-    },{
-        txt: 'Material UI', to: 'materialUI'
-    },{
-        txt: 'Swiper', to: 'swiper'
-    },{
-        txt: '外部文字嵌入',  to: 'font'
-    }]
-}]
-
 function Navbar() {
+    const navList = useSelector((state) => state.navList).navList
     const [titleIndex, setTitleIndex] = useState(0)
     const [chooseTitleIndex, setChooseTitleIndex] = useState(0)
     const [chooseSubTitleIndex, setChooseSubTitleIndex] = useState(0)
--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from './redux/todoSlice'
 import todoAsyncReducer from './redux/todoAsyncSlice'
 import todoAxiosReducer from './redux/todoAxiosSlice'
+import navListReducer from './redux/navListSlice'
 
 export const store = configureStore({
   reducer: {
       todoList: todoReducer,
       todoAsync: todoAsyncReducer,
-      todoAxios: todoAxiosReducer
+      todoAxios: todoAxiosReducer,
+      navList: navListReducer
   },
 })
