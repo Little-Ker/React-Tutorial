32b3fd3acf5fac9d9af95dc2f5cf109c60a39686
Vivi <vivichi@yile.com.tw>
2024-01-30 11:21:48 +0800

    [新增] prismjs 套件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css'
-import React from "react"
+import React, { useEffect } from "react"
 import ArticalView from './view/articleView/ArticalView'
+import './style/prism.css'
+import Prism from 'prismjs'
 
 function App() {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
+
   return (
     <div className="App">
       <ArticalView />
